831e36bc31fa9a37b75e4e6ca905ae9287804aff
Alexander Park <parkalex380@gmail.com>
2021-08-03 12:54:29 -0500

    forgot to add files for routes to git so committing again :/

---
+++ pages/profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function profile() {
+    return (
+        <div>
+            <h1>Profile Page: WIP</h1>
+        </div>
+    )
+}
---
+++ pages/upload.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function upload() {
+    return (
+        <div>
+            <h1>File Upload Page: WIP</h1>
+        </div>
+    )
+}
